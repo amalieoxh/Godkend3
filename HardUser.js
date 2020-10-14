e08887735b5077488527502982b4be9d504ee363
Amalie Oxholm <amalieoxholm@Amalies-MacBook-Pro-2.local>
2020-10-14 12:00:19 +0200

    first commit

---
+++ HardUser.js
@@ -0,0 +1,12 @@
+//henter User model
+const User = require('./Model/user')
+
+let userArray = [] 
+let User1 = new User('Christian', [1997, 10, 09], 'Male', 'Copenhagen', 'christian@mail.dk', '1234', ['hockey', 'Pizza', 'cat videoes']);
+let User2 = new User('Bo', [1998, 10, 02], 'Male', 'Copenhagen', 'bo@mail.dk', 'AB1234', ['soccer', 'travelling', 'cat videoes']);
+
+userArray = [User1, User2]
+
+module.exports = userArray
+
+# Godkend3
